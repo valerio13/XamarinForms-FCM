50eca385f3ac63f8a148a184e63d93de848a9c8f
Valerio <valerio.colantonio@gmail.com>
2021-12-21 15:29:17 +0100

    Added Android project

---
+++ TestFCM.Android/MyFirebaseIIDService.cs
@@ -0,0 +1,36 @@
+﻿using Android.App;
+using Firebase.Iid;
+
+namespace TestFCM.Droid
+{
+    [Service]
+    [IntentFilter(new[] { "com.google.firebase.INSTANCE_ID_EVENT" })]
+    public class MyFirebaseIIDService : FirebaseInstanceIdService
+    {
+        const string TAG = "MyFirebaseIIDService";
+        //        public override void OnTokenRefresh()
+        //        {
+        //            var refreshedToken = FirebaseInstanceId.Instance.Token;
+        //            SendRegistrationToServer(refreshedToken);
+        //        }
+
+        //        void SendRegistrationToServer(string token)
+        //        {
+        //            // send this token to server
+        //        }
+        //    }
+        //}
+
+
+
+        public override void OnTokenRefresh()
+        {
+            // Get updated InstanceID token.
+            var refreshedToken = FirebaseInstanceId.Instance.Token;
+            Android.Util.Log.Debug(TAG, "Refreshed token: " + refreshedToken);
+            System.Diagnostics.Debug.WriteLine($"######Token######  :  {refreshedToken}");
+            Xamarin.Forms.Application.Current.Properties["Fcmtocken"] = FirebaseInstanceId.Instance.Token ?? "";
+            Xamarin.Forms.Application.Current.SavePropertiesAsync();
+        }
+    }
+}
---
+++ TestFCM.Android/MyFirebaseMessagingService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Firebase.Messaging;
+
+namespace TestFCM.Droid
+{
+    public class MyFirebaseMessagingService : FirebaseMessagingService
+    {
+        public MyFirebaseMessagingService()
+        {
+
+        }
+        public override void OnMessageReceived(RemoteMessage message)
+        {
+            base.OnMessageReceived(message);
+            new NotificationHelper().CreateNotification(message.GetNotification().Title, message.GetNotification().Body);
+        }
+    }
+}
